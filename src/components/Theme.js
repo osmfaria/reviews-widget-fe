73cd31dfc94056e37afa6b73b9e42c67f7331aed
Osmar Faria <82468282+osmfaria@users.noreply.github.com>
2023-11-22 15:46:22 -0800

    feat: implement mui theme

---
+++ src/components/Theme.js
@@ -0,0 +1,19 @@
+'use client'
+
+import { createTheme } from '@mui/material/styles'
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#F3F3F3',
+      secondary: '#b8b7ad',
+    },
+    secondary: {
+      main: '#47E10C',
+    },
+    background: {
+      default: '#252833',
+      paper: '#373a47',
+    },
+  },
+})
