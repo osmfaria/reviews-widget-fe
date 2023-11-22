6382844c7ef770dfaafafbd71160f365b3e3bad9
Osmar Faria <82468282+osmfaria@users.noreply.github.com>
2023-11-22 15:45:58 -0800

    feat: add Theme, toast config and baseline for css in material ui

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,9 @@
+import { Box, CssBaseline, Stack } from '@mui/material'
 import { Inter } from 'next/font/google'
-import './globals.css'
+import { Flip, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { ThemeProvider } from '@mui/material'
+import { theme } from '@/components/Theme'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +15,37 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <ToastContainer
+            position='top-center'
+            autoClose={2000}
+            limit={1}
+            hideProgressBar={true}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss={false}
+            draggable
+            pauseOnHover={false}
+            theme='light'
+            transition={Flip}
+          />
+          <Stack
+            direction='column'
+            justifyContent='space-between'
+            minHeight='100vh'
+          >
+            <Box
+              minHeight='80vh'
+              sx={{ padding: { xs: '20px 0', sm: '60px 0' } }}
+            >
+              {children}
+            </Box>
+          </Stack>
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
